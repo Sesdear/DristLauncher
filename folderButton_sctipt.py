fccc13a0df79bfa9678ea10aab92268e69192be5
HLNikNiky <56186901+Sesdear@users.noreply.github.com>
2024-04-18 00:41:51 +0700

    del any

--- folderButton_sctipt.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-def folder_open():
-    # Открывает директорию кода
-    current_directory = os.getcwd()
-    subprocess.Popen(r'explorer "{}"'.format(current_directory))
